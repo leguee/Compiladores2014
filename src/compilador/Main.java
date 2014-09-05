f76eacddddced29ea1793dc801b0fd35d04cd778
legui <fnleguizamon@gmail.com>
2014-09-05 15:30:27 +0200

    se agrego main

---
+++ src/compilador/Main.java
@@ -0,0 +1,37 @@
+package compilador;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+public class Main extends javax.swing.JFrame {
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Main inst = new Main();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public Main() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
